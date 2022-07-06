82642a74369ff737217a3fa3aa90b714294e5ddc
yuetloo <yuetloo@users.noreply.github.com>
2022-07-06 04:33:28 +0000

    deploy: 80c68d496e07cdf6a171c1a6b0ed2e4960dae050

--- _next/static/UVsvrAO6SeEi68L020PZL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/149b18973e5508c7.css","static/chunks/pages/index-4c9babb78b38734c.js"],"/_error":["static/chunks/pages/_error-942918685354aae9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
